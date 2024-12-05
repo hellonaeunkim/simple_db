a1e7d3e798bc844e6522be2e06c9eaf343eddb40
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-05 10:51:20 +0900

    feat : DB 연결하여 Connection 객체 생성 & @BeforeAll에서 article table 생성 method 추가

--- src/main/java/com/ann/simpleDb/SimpleDb.java
+++ src/main/java/com/ann/simpleDb/SimpleDb.java
@@ -2,6 +2,8 @@ package com.ann.simpleDb;
 
 import lombok.RequiredArgsConstructor;
 
+import java.sql.*;
+
 // final field의 생성자 자동으로 생성
 @RequiredArgsConstructor
 public class SimpleDb {
@@ -9,4 +11,37 @@ public class SimpleDb {
     private final String username;
     private final String password;
     private final String dbName;
+    private Connection connection;
+
+    // Database 연결 초기화
+    private void connect() {
+        if (connection == null) {
+            String url = String.format("jdbc:mysql://%s/%s?useSSL=false", host, dbName);
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                throw new RuntimeException("Failed to connect to database", e);
+            }
+        }
+    }
+
+    // SQL 실행 method
+    public void run(String sql) {
+        connect(); // 연결 초기화
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to execute SQL: " + sql, e);
+        }
+    }
+    // 자원 해제
+    public void close() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException("Failed to close database connection", e);
+            }
+        }
+    }
 }
--- src/test/java/com/ann/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ann/simpleDb/SimpleDbTest.java
@@ -1,8 +1,6 @@
 package com.ann.simpleDb;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 // method 실행 순서 지정 : SimpleDbTest class의 모든 test method가 이름의 알파벳 순서대로 실행
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -12,6 +10,29 @@ public class SimpleDbTest {
     @BeforeAll
     public static void beforeAll() {
         // Database 정보
-        simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+        simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb_test");
+
+        createArticleTable();
+    }
+
+    private static void createArticleTable() {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
+    }
+
+    @Test
+    @DisplayName("데이터베이스 연결 테스트")
+    public void t000() {
     }
 }
