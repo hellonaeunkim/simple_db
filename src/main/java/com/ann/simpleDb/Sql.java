f1457ed7fbea33c1c22933b5e0c4801f0757af5b
hellonaeunkim <hellonaeunkim@gmail.com>
2024-12-05 12:39:44 +0900

    feat : insert method 구현 (t002)

--- src/main/java/com/ann/simpleDb/Sql.java
+++ src/main/java/com/ann/simpleDb/Sql.java
@@ -1,15 +1,16 @@
 package com.ann.simpleDb;
 
 public class Sql {
-    public Sql append(String sqlBit) {
-        return this;
-    }
 
-    public Sql append(String sqlBit, Object param) {
+    public Sql append(String sqlBit, Object... params) {
         return this;
     }
 
     public long insert() {
         return 1;
+    }
+
+    public int update() {
+        return 3;
     }
 }
--- src/test/java/com/ann/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ann/simpleDb/SimpleDbTest.java
@@ -75,13 +75,37 @@ public class SimpleDbTest {
         body = '내용 new'
         */
         sql.append("INSERT INTO article")
-                .append("SET createdDate = NOW()")
-                .append(", modifiedDate = NOW()")
-                .append(", title = ?", "제목 new")
-                .append(", body = ?", "내용 new");
+            .append("SET createdDate = NOW()")
+            .append(", modifiedDate = NOW()")
+            .append(", title = ?", "제목 new")
+            .append(", body = ?", "내용 new");
 
         long newId = sql.insert(); // AUTO_INCREMENT 에 의해서 생성된 주키 리턴
 
         assertThat(newId).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("update")
+    public void t002() {
+        Sql sql = simpleDb.genSql();
+
+        // id가 0, 1, 2, 3인 글 수정
+        // id가 0인 글은 없으니, 실제로는 3개의 글이 삭제됨
+
+        /*
+        == rawSql ==
+        UPDATE article
+        SET title = '제목 new'
+        WHERE id IN ('0', '1', '2', '3')
+        */
+        sql.append("UPDATE article")
+            .append("SET title = ?", "제목 new")
+            .append("WHERE id IN (?, ?, ?, ?)", 0, 1, 2, 3);
+
+        // 수정된 row 개수
+        int affectedRowsCount = sql.update();
+
+        assertThat(affectedRowsCount).isEqualTo(3);
     }
 }
